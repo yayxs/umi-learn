db7d46544c3aae29391bddcee511601c42302249
yayxs <leaderywl@163.com>
2020-06-15 20:57:54 +0800

    feat(add new admin): 新增umi后台
    初始化umiadmin后台

--- nest-api-service/src/config/db.ts
+++ nest-api-service/src/config/db.ts
@@ -1,11 +1,11 @@
 import { join } from 'path';
 export default {
   type: 'mysql',
-  host: 'localhost',
+  host: '62.234.111.140',
   port: 3306,
-  username: 'root',
-  password: '123456',
-  database: 'blog',
+  username: 'umi-nest',
+  password: '',
+  database: 'umi-nest',
   entities: [join(__dirname, '../', '**/**.entity{.ts,.js}')], // 使用实体
   synchronize: true,
 };
